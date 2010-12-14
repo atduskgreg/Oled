f6c9f45c07557f68b69a1b64cb15ea836ae020a0
Greg Borenstein <greg@mfdz.com>
2010-12-14 01:37:11 -0500

    first commit

---
+++ oled.cpp
@@ -0,0 +1,396 @@
+#include "oled.h"
+
+Oled::Oled(Print *serial){
+    _serial = serial;
+}
+
+
+// Initialise OLED display. You must first activate a _serial comunication!
+
+void Oled::init()
+
+{
+
+  // First reset display
+  resetDisplay();  
+
+  delay(OLED_INITDELAYMS);                       // Wait for init
+
+  // Autodetect baudrate
+
+  _serial->print(OLED_DETECT_BAUDRATE, BYTE); 
+
+  getResponse(); 
+
+
+
+}
+
+
+void Oled::resetDisplay()
+
+{
+
+  digitalWrite(OLED_RESETPIN, LOW);
+
+  delay(20);                  
+
+  digitalWrite(OLED_RESETPIN, HIGH);
+
+  delay(20);  
+
+
+
+}
+
+
+
+char Oled::getResponse()
+
+{
+
+  byte incomingByte = OLED_ACK;
+
+
+
+  // Wait for data avaliable
+
+  while (!_serial->available()) { 
+    delay(1); 
+  }
+
+
+
+  // Read incoming byte
+
+  incomingByte = _serial->read();
+
+
+
+  return incomingByte;
+
+}
+
+
+
+
+
+
+// Get 16bits value from RGB (0 to 63, 565 format)
+
+int Oled::getRGB(int red, int green, int blue)
+
+{
+
+
+
+  char text[255];
+
+
+
+  int outR = ((red * 31) / 255);
+
+  int outG = ((green * 63) / 255);
+
+  int outB = ((blue * 31) / 255);
+
+
+
+  return (outR << 11) | (outG << 5) | outB;
+
+}
+
+
+
+void Oled::clear()
+
+{
+
+
+
+  _serial->print(OLED_CLEAR, BYTE); // Pixel write
+
+  delay(20);
+
+  getResponse();
+
+
+
+}
+
+
+
+void Oled::putPixel(char x, char y, int color)
+
+{
+
+
+
+  // Putpixel
+
+  _serial->print(OLED_PUTPIXEL, BYTE); // Pixel write
+
+  _serial->print(x, BYTE);	// x
+
+  _serial->print(y, BYTE);
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);		// LSB
+
+
+
+  getResponse();
+
+
+
+}
+
+
+
+void Oled::drawLine(char x1, char y1, char x2, char y2, int color)
+
+{
+
+
+
+  // Line drawing
+
+  _serial->print(OLED_LINE, BYTE); // Line
+
+
+
+  _serial->print(x1, BYTE);	// x1
+
+  _serial->print(y1, BYTE);		// y1
+
+  _serial->print(x2, BYTE);	// x2
+
+  _serial->print(y2, BYTE);		// y2
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);		// LSB
+
+
+
+  getResponse();
+
+
+
+}
+
+
+
+void Oled::drawRectangle(char x, char y, char width, char height, char filled, int color)
+
+{
+
+
+
+  //int color = 0xFFFF;
+
+
+
+  // Rectangle drawing
+
+  _serial->print(OLED_RECTANGLE, BYTE); 
+
+
+
+  _serial->print(x, BYTE);			// x1
+
+  _serial->print(y, BYTE);		// y1
+
+
+
+  _serial->print(x+width, BYTE);		// x2
+
+  _serial->print(y+height, BYTE);		// y1
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);		// LSB
+
+  /*
+
+   	if (filled == 1) { _serial->print(0x01); }	// Filled
+   
+   	else { _serial->print(0x00); }				// Outline
+   
+   	*/
+
+  getResponse();
+
+}
+
+
+
+void Oled::drawCircle(char x, char y, char radius, char filled, int color)
+
+{
+
+  _serial->print(OLED_CIRCLE, BYTE); 
+
+
+
+  _serial->print(x, BYTE);	// x
+
+  _serial->print(y, BYTE);	// y
+
+  _serial->print(radius, BYTE);	// radius
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);		// LSB
+
+
+
+  getResponse();
+
+
+
+}
+
+
+
+/*
+
+ Change font format:
+ 
+ FontType can be:
+ 
+ OLED_FONT5X7
+ 
+ OLED_FONT8X8
+ 
+ OLED_FONT8X12
+ 
+ */
+
+void Oled::setFontSize(char FontType)
+
+{
+
+  _serial->print(OLED_SETFONTSIZE, BYTE);
+
+  _serial->print(FontType, BYTE);
+
+
+
+  getResponse();
+
+}
+
+
+
+void Oled::drawText(char column, char row, char font_size, char *mytext, int color)
+
+{
+
+  //char mytext[] = "Arkanoid by pK";
+
+
+
+  _serial->print(0x73, BYTE); // s (Formated text)
+
+
+
+  //_serial->print(column); // column
+
+
+
+  // Adjust to center of the screen (26 Columns at font size 0)
+
+  int newCol = 13 - (strlen(mytext)/2);
+
+  //_serial->print(newCol); // column
+
+  _serial->print(column, BYTE); // column
+
+
+
+  _serial->print(row, BYTE); // row
+
+  _serial->print(font_size, BYTE); // font size (0 = 5x7 font, 1 = 8x8 font, 2 = 8x12 font)
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);		// LSB
+
+
+
+  for (int i=0 ; i<strlen(mytext) ; i++)
+
+  {
+
+    _serial->print(mytext[i], BYTE); // character to write
+
+  }
+
+  _serial->print(0x00, BYTE); // string terminator (always 0x00)
+
+
+
+  getResponse();
+
+
+
+}
+
+
+
+void Oled::drawSingleChar(char column, char row, char font_size, char MyChar, int color)
+
+{
+
+
+
+  _serial->print(OLED_TEXTFORMATED, BYTE); // T (Formated)
+
+
+
+  _serial->print(MyChar, BYTE);
+
+  _serial->print(column, BYTE);
+
+  _serial->print(row, BYTE);
+
+
+
+  // Color
+
+  _serial->print(color >> 8, BYTE);		// MSB			
+
+  _serial->print(color & 0xFF, BYTE);	// LSB
+
+
+
+  getResponse();
+
+
+
+}
+
+
+
---
+++ oled.h
@@ -0,0 +1,117 @@
+/*******************************************************
+ * uOLED-160-GMD1 Driver for Arduino
+ * 
+ * Updated by Mustafa Bagdatli
+ * http://mustafabagdatli.com
+ * 12/02/2010
+ * 
+ * Original code by Oscar Gonzalez
+ * www.bricogeek.com
+ * August 2007
+ * 
+ * 
+ * 
+ * 
+ *******************************************************/
+
+#ifndef Oled_h
+#define Oled_h
+
+
+#define OLED_BAUDRATE                                           9600//57600
+
+#define OLED_RESETPIN                                           8      // PIN of reset
+
+#define OLED_INITDELAYMS                                        5000
+
+
+
+// Initialisation routine
+
+#define	OLED_DETECT_BAUDRATE					0x55
+
+
+
+// Drawing routines
+
+#define	OLED_CLEAR						0x45
+
+#define	OLED_BKGCOLOR						0x42
+
+#define	OLED_COPYPASTE						0x63
+
+
+
+#define	OLED_LINE						0x4C
+
+#define	OLED_CIRCLE						0x43
+
+#define	OLED_CIRCLEFILL						0x69
+
+#define	OLED_PUTPIXEL						0x50
+
+#define	OLED_READPIXEL						0x52
+
+#define	OLED_RECTANGLE						0x72
+
+#define	OLED_PAINTAREA						0x70
+
+
+
+// Text properties
+
+#define	OLED_SETFONTSIZE					0x46
+
+#define	OLED_FONT5X7					0x01
+
+#define	OLED_FONT8X8					0x02
+
+#define	OLED_FONT8X12					0x03
+
+#define	OLED_TEXTFORMATED					0x54
+
+
+
+// OLED Control
+
+#define	OLED_COMMAND_CONTROL					0x59
+
+#define	OLED_COMMAND_DISPLAY					0x01
+
+#define	OLED_COMMAND_CONTRAST					0x02
+
+#define	OLED_COMMAND_POWER					0x03
+
+
+
+#define OLED_ACK  0x06  // Ok
+
+#define OLED_NAK  0x15  // Error
+
+#include "Print.h"
+#include "WProgram.h"
+
+class Oled
+{
+private:
+  // per object data
+  Print *_serial;
+
+public:
+    void resetDisplay();
+    char getResponse();
+    Oled(Print *serial);
+    void init();
+    int getRGB(int red, int green, int blue);
+    void clear();
+    void putPixel(char x, char y, int color);
+    void drawLine(char x1, char y1, char x2, char y2, int color);
+    void drawRectangle(char x, char y, char width, char height, char filled, int color);
+    void drawCircle(char x, char y, char radius, char filled, int color);
+    void setFontSize(char FontType);
+    void drawText(char column, char row, char font_size, char *mytext, int color);
+    void drawSingleChar(char column, char row, char font_size, char MyChar, int color);
+
+};
+
+#endif
